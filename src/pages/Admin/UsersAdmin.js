f3a86a7be29b9560da6932d4a7d9c9248c5a5df3
Cesar Morales <cmorales@thedataage.com>
2021-12-23 11:03:31 -0600

    Creation of modal for new user

--- src/pages/Admin/UsersAdmin.js
+++ src/pages/Admin/UsersAdmin.js
@@ -1,22 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Loader } from "semantic-ui-react"
 
-import { HeaderPage, TableUsers } from "../../components/Admin"
+import { ModalBasic } from "../../components/Common"
+import { HeaderPage, TableUsers, AddEditUserForm } from "../../components/Admin"
 import { useUser } from "../../hooks"
 
 export function UsersAdmin() {
+    const [showModal, setShowModal] = useState(false)
+    const [titleModal, setTitleModal] = useState(null)
+    const [contentModal, setContentModal] = useState(null)
     const { loading, users, getUsers } = useUser()
 
     useEffect(() => getUsers(), []);
 
+    const openCloseModal = () => setShowModal((prev) => !prev)
+
+    const addUser = () => {
+        setTitleModal("Nuevo usuario")
+        setContentModal(<AddEditUserForm />)
+        openCloseModal()
+    }
+
     return (
-        <div>
-            <HeaderPage title="Usuarios" btnTitle="Nuevo usuario" />
+        <>
+            <HeaderPage title="Usuarios" btnTitle="Nuevo usuario" btnClick={addUser} />
             {loading ? (
                 <Loader active inline="centered">Cargando...</Loader>
             ) : (
                 <TableUsers users={users} />
             )}
-        </div>
+
+            <ModalBasic
+                show={showModal}
+                onClose={openCloseModal}
+                title={titleModal}
+                children={contentModal}
+            />
+        </>
     )
 }
